152711a7da70c9bd43087222b5d7c1c5732432f8
C0ldHands <48500351+C0ldHands@users.noreply.github.com>
2020-11-15 18:20:35 -0500

    Finished trelloyes project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import List from './list.js'
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className='App'>
+      <header className = 'App-header'>
+        <h1>Trelloyes!</h1>
       </header>
-    </div>
+      <div className = 'App-list'>
+        {props.store.lists.map((list)=>{return <List header = {list.header} cards = {list.cardIds.map(id => props.store.allCards[id])} />;})}
+      </div>
+    </main>
   );
+
 }
 
 export default App;
---
+++ src/list.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './card.js';
+import './list.css';
+
+function List(props){
+    const listCards = props.cards.map((card,i)=> <Card title = {card.title} content = {card.content} />)
+    return(
+        <section className='List'>
+            <header className='List-header'>
+                <h2>{props.header}</h2>
+            </header>
+            <div className='List-cards'>
+                {listCards}
+                <button type = 'button' className='List-add-button'>+ Add Random Card</button>
+            </div>
+        </section>
+    );
+}
+
+export default List;
