a51198ebc47ad5ac81aa974015c4609a5aa3424f
C0ldHands <48500351+C0ldHands@users.noreply.github.com>
2020-11-20 20:30:58 -0500

    Finished newRandomCard function, removed comments

--- src/App.js
+++ src/App.js
@@ -11,48 +11,46 @@ class App extends React.Component {
     return rest;
   }
 
-  // handleDeleteItem(id){
-  //   console.log('handle delete item called', { id })
-  //   // const stateObject = this.state.allCards;
-  //   // this.omit(stateObject,id)
-  //   console.log(this.state.allCards)
-  //   // this.setState()
-  // }
-  // // handleCheckItem(item){
-  // //   console.log('handle check item called', { item })
-  // // }
-
-  // idMatch(id){
-  //   return
-  // }
-
   handleDeleteItem = (id) => {
-    console.log('handle delete item called', { id })
-    // const newList = this.state.lists.cardIds.filter(cardId => cardId !== id)
-    // this.setState({
-    //   cardIds: newList
+    // this.state.lists.map((list,i)=>{
+    //   this.state.lists[i] = {id: i, header: list.header, cardIds: list.cardIds.filter(cardId => {return cardId !== id})}
     // })
-    this.state.lists.map((list,i)=>{
-      // list.cardIds.filter(cardId => cardId !== id)
-      this.state.lists[i] = {id: i, header: list.header, cardIds: list.cardIds.filter(cardId => {return cardId !== id})}
+    const newLists = this.state.lists
+    newLists.map((list,i)=>{
+      newLists[i] = {id: i, header: list.header, cardIds: list.cardIds.filter(cardId => {return cardId !== id})}
     })
+    this.setState({lists:newLists})
     this.setState({allCards:this.omit(this.state.allCards,id)})
-    console.log(this.omit(this.state.allCards,id))
   }
 
-  handleAddRandomCard(listID){
-    console.log('handle random card called')
+  newRandomCard = () => {
+    const id = Math.random().toString(36).substring(2, 4)
+      + Math.random().toString(36).substring(2, 4);
+    return {
+      id,
+      title: `Random Card ${id}`,
+      content: 'lorem ipsum',
+    }
+  }
+
+  handleAddRandomCard = (listID) => {
+    const newCard = this.newRandomCard();
+    const id = newCard.id
+    const newCards = this.state.allCards
+    newCards[id]= newCard;
+    this.setState({allCards:newCards})
+    this.state.lists[listID].cardIds.push(id)
   }
 
   render (){
-    console.log(this.state.lists)
     return (
       <main className='App'>
         <header className = 'App-header'>
           <h1>Trelloyes!</h1>
         </header>
         <div className = 'App-list'>
-          {this.state.lists.map((list)=>{return <List 
+          {this.state.lists.map((list,i)=>{return <List 
+            id = {i}
             header = {list.header} 
             cards = {list.cardIds.map(id => this.state.allCards[id])} 
             onDeleteItem={this.handleDeleteItem}
